758828f5b7fb4007703490e8e7684eea4585a343
wjhuang98 <williamhuang988@gmail.com>
2024-02-13 01:39:13 -0500

    added neue font and added more text to front page

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,20 @@ function App() {
       {/* bg-[#181818] dark */}
       {/* bg-[#d7c9b0] beige */}
       <div className="flex flex-col bg-[#181818] h-screen justify-center items-center">
-        <p className="h-1/2 w-1/2 text-2xl font-mono text-[#fbf8e7]">
-          Hello I'm William, you can call me Willy.
-        </p>
+        <div className="h-1/2 w-1/2 text-4xl font-semibold font-neue text-[#fbf8e7]">
+          <p>
+            Hello, I'm William
+          </p>
+          <p className="pt-2">
+            <span className="text-[#81beac]">|</span> About
+          </p>
+          <p className="pt-2">
+            <span className="text-[#eda435]">|</span> Projects
+          </p>
+          <p className="pt-2">
+            <span className="text-[#942724]">|</span> Contacts
+          </p>
+        </div>
         <div className="flex flex-col fixed bottom-0 w-full">
           <div className="h-3 w-full bg-[#81beac]"></div>
           <div className="h-3 w-full bg-[#eda435]"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.html', './src/**/*.js', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        'neue': ['Neue Haas Grotesk', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
