601042c1819fa151fabb341253bf6ac955998a9c
wjhuang98 <williamhuang988@gmail.com>
2024-02-20 00:19:40 -0500

    added function to take place of hrefs, fixed url issue

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,11 @@
 export default function Home() {
+    const goTo = (id: string) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <div id="Home" className="flex flex-col bg-lt-black h-screen justify-center items-center overflow-hidden relative snap-start">
             <div className="h-1/2 w-1/2 text-4xl font-semibold font-neue text-lt-white">
@@ -6,13 +13,13 @@ export default function Home() {
                     Hello, I'm William
                 </p>
                 <p className="pt-2">
-                    <span className="text-lt-blue">|</span> <a href="#About" className="hover:text-lt-blue duration-300">About</a>
+                    <span className="text-lt-blue">|</span> <button onClick={() => goTo('About')} className="hover:text-lt-blue duration-300">About</button>
                 </p>
                 <p className="pt-2">
-                    <span className="text-lt-yellow">|</span> <a href="#Projects" className="hover:text-lt-yellow duration-300">Projects</a>
+                    <span className="text-lt-yellow">|</span> <button onClick={() => goTo('Projects')} className="hover:text-lt-yellow duration-300">Projects</button>
                 </p>
                 <p className="pt-2">
-                    <span className="text-lt-red">|</span> <a href="#Contacts" className="hover:text-lt-red duration-300">Contacts</a>
+                    <span className="text-lt-red">|</span> <button onClick={() => goTo('Contacts')} className="hover:text-lt-red duration-300">Contacts</button>
                 </p>
             </div>
         </div>
